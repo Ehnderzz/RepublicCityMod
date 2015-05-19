b6f9d8840010af7efc758ce87dd3d5cfea3decc2
Ehnderzz <Ehnderzz@gmail.com>
2015-05-19 14:35:57 -0400

    Getting used to Git
    Give me a second to adjust

--- src/com/tlaminecraft/RepublicCityMod/Main.java
+++ src/com/tlaminecraft/RepublicCityMod/Main.java
@@ -1,5 +1,31 @@
 package com.tlaminecraft.RepublicCityMod;
 
-public class Main {
+import net.milkbowl.vault.economy.Economy;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin implements Listener{
+	
+	 public static Economy economy = null;
+	 
+	 private boolean setupEconomy(){
+	        RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+	        if (economyProvider != null) {
+	            economy = economyProvider.getProvider();
+	        }
+
+	        return (economy != null);
+	    }
+	 
+	 public void onEnalbe(){
+		 if(!setupEconomy()){
+			 getLogger().severe("PLUGIN REQUIRES VAULT, ERROR SETTING UP ECONOMY");
+			 Bukkit.getPluginManager().disablePlugin(this);
+			 
+		 }
+	 }
 }
